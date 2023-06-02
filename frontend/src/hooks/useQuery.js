8ee07915cc43da9cb867ac47d73083f5929d7cba
Tanveerhusyn <tanveerhussain465@gmail.com>
2023-06-03 03:06:32 +0500

    minor changes

--- frontend/src/hooks/useQuery.js
+++ frontend/src/hooks/useQuery.js
@@ -9,7 +9,7 @@ function useQuery() {
   const [error, setError] = useState(null);
 
   const baseURL = process.env.NODE_ENV=="production"?process.env.REACT_APP_API_URL:"http://localhost:5000";
-
+  console.log(process.env.REACT_APP_API_URL)
  
 
   useEffect(() => {
